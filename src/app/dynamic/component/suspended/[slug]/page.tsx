45e4fd861fe2c00a20eef433928ba20339a7509c
108yen <kazuking.1911@gmail.com>
2026-01-11 23:32:14 +0900

    feat: added suspended

---
+++ src/app/dynamic/component/suspended/[slug]/page.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/button"
+import { Cached, CachedRemote } from "@/cached/component"
+import { Card } from "@/components/card"
+import { CreatedAt } from "@/components/duration"
+import { LoadingCard } from "@/components/loading-card"
+import { PageLayout } from "@/components/page-layout"
+import { VStack } from "@/components/stack"
+import { updateTag } from "next/cache"
+import { Suspense } from "react"
+
+export default async function Page() {
+  return (
+    <PageLayout>
+      <VStack>
+        <h1>Dynamic Page</h1>
+        <Suspense fallback={<div>Loading...</div>}>
+          <CreatedAt />
+        </Suspense>
+      </VStack>
+
+      <Card>
+        <h2>Without Parameters</h2>
+
+        <Suspense fallback={<LoadingCard />}>
+          <Cached />
+        </Suspense>
+
+        <Suspense fallback={<LoadingCard />}>
+          <CachedRemote />
+        </Suspense>
+      </Card>
+
+      <Button
+        onClick={async () => {
+          "use server"
+          updateTag("component")
+        }}
+      >
+        Purge Components Cache
+      </Button>
+    </PageLayout>
+  )
+}
